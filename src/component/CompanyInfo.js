7ced637fd04893fcfbc34cfdcda8c9a29e200516
raghuveerchintal <raghuveerchintal8@gmail.com>
2020-11-01 23:21:47 +0530

    CompanyInfo_updated

--- src/component/CompanyInfo.js
+++ src/component/CompanyInfo.js
@@ -20,10 +20,28 @@ const CompanyInfo = () =>{
             <div>
                 <NavbarAdmin></NavbarAdmin>
             </div>
+            <div className="my-4 ml-4">
+              <h3 className="font-weight-bold">Company-Wise-Rating</h3>
+            </div>
             <div>
                 {
                     companyrating && Object.keys(companyrating).map((companyname)=>{
-                    return <h3>{companyname}:{companyrating[companyname]}</h3>
+                    return (
+                        
+                        <div className="col-md-6 col-12 mx-auto">
+                        <div className="card-deck">
+                            <div className="card mb-2">
+                                <div className="card-body">
+                                    <h4 className="card-title font-weight-bold">Company Name : {companyname}</h4>
+                                </div>
+                                <div className="card-footer bg-secondary">
+                                    <h5 className="text-light">Rating : {companyrating[companyname]}</h5>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+
+                    )
 
                     })
                 }
